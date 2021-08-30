b0e883107bc9a6aca913d002d5ba2c2e58ddb136
Jin Mac jinctmac@gmail.com <jinctmac@gmail.com>
2021-08-30 17:55:36 +0100

    added recipeint id

--- app/models/user_review.rb
+++ app/models/user_review.rb
@@ -1,3 +1,8 @@
 class UserReview < ApplicationRecord
   belongs_to :user
+  # belongs to recipient, but is looking for user
+  # can do userreview.user to get user who made review
+  # and can do userreview.recipient to get the user receiving review
+  # can use .recipient to get and set
+  belongs_to :recipient, class_name: 'User'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_30_160648) do
+ActiveRecord::Schema.define(version: 2021_08_30_164739) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,8 @@ ActiveRecord::Schema.define(version: 2021_08_30_160648) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "recipient_id"
+    t.index ["recipient_id"], name: "index_user_reviews_on_recipient_id"
     t.index ["user_id"], name: "index_user_reviews_on_user_id"
   end
 
