89e952b2e40e8e83f87109c62e326002339339ae
Jin Mac jinctmac@gmail.com <jinctmac@gmail.com>
2021-09-01 17:22:29 +0100

    rating stars thing

---
+++ app/javascript/plugins/init_star_rating.js
@@ -0,0 +1,9 @@
+// import the jQuery plugin and its JS functionality
+import "jquery-bar-rating";
+import $ from 'jquery'; // <-- if you're NOT using a Le Wagon template (cf jQuery section)
+
+const initStarRating = () => {
+  // TODO
+};
+
+export { initStarRating };
