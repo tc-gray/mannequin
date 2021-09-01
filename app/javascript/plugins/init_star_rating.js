71f78ad8911d0aed7046b73747cdc8ec96dd02e7
Jin Mac jinctmac@gmail.com <jinctmac@gmail.com>
2021-09-01 18:08:56 +0100

    added review functionality, still missing edit/destroy

--- app/javascript/plugins/init_star_rating.js
+++ app/javascript/plugins/init_star_rating.js
@@ -4,6 +4,12 @@ import $ from 'jquery'; // <-- if you're NOT using a Le Wagon template (cf jQuer
 
 const initStarRating = () => {
   // TODO
+  // the below is the equivalent of doing
+  // document.querySelector('#product_review_rating')
+  // or document.getElementById('product_review_rating')
+  $('#product_review_rating').barrating({
+    theme: 'css-stars'
+  });
 };
 
 export { initStarRating };
