66569427bf531f190fcd96d52c5c3c1b56fd0866
stephanie ezimoha <stephanie.ezimoha@yahoo.co.uk>
2021-09-03 12:30:09 +0100

    Added ability for other users to see their booking status and for only user that owns product to be able to accept + decline

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
   def show
-    @user = current_user
-    @pending_bookings = Booking.where(status: "Pending")
+    @pending_bookings = Booking.where(status: "Pending", user: current_user)
   end
 
   def profile
