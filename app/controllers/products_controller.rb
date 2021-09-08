1cac4e30e3fe1d0d67dbbd2ce144cae16748a158
Tegan Cadney-Gray <tegan.cadney@gmail.com>
2021-09-08 18:10:34 +0100

    conflicts resolved

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,21 +2,54 @@ class ProductsController < ApplicationController
   before_action :find_product, only: [:show, :edit, :update, :destroy]
 
   def index
+    # products = Product.all
+
+
+
+
     if params[:query].present?
       @products = Product.search_by_name_and_description_and_size_and_category(params[:query])
       session[:search_query] = params[:query]
     elsif params[:category].present?
       if session[:search_query]
         @search = Product.search_by_name_and_description_and_size_and_category(session[:search_query])
-        @products = @search.where("category ILIKE ?", "%#{params[:category]}%")
+        products = @search.where("category ILIKE ?", "%#{params[:category]}%")
+        if params["sort-by"].present?
+          if params["sort-by"] == "accending"
+            @products = products.sort_by(&:price_cents)
+          else
+            @products = products.sort_by(&:price_cents).reverse
+          end
+        else
+          @products = @search.where("category ILIKE ?", "%#{params[:category]}%")
+        end
       else
-        # @products = Product.search_by_name_and_description_and_size_and_category(params[:category])
-        @products = Product.where("category ILIKE ?", "%#{params[:category]}%")
+        products = Product.where("category ILIKE ?", "%#{params[:category]}%")
         session.delete("search_query")
+        if params["sort-by"].present?
+          if params["sort-by"] == "accending"
+            @products = products.sort_by(&:price_cents)
+          else
+            @products = products.sort_by(&:price_cents).reverse
+          end
+        else
+          @products = Product.where("category ILIKE ?", "%#{params[:category]}%")
+        end
       end
     else
-      @products = Product.all
+      products = Product.all
+      if params["sort-by"].present?
+        if params["sort-by"] == "accending"
+          @products = products.sort_by(&:price_cents)
+        else
+          @products = products.sort_by(&:price_cents).reverse
+        end
+      else
+        @products = Product.all
+      end
     end
+
+
   end
 
   def show
